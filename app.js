c0ea20dc37a31f63820b675bad903bf01159d673
nyeoro-bong <ogasawara4039@gmail.com>
2020-08-08 12:33:44 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
+function fib(n){
+    if (memo.has(n)){
+        return memo.get(n);
+    }
+    const value= fib(n-2) + fib(n-1);
+    memo.set(n,value);
+    return value;
+}
+
+
+const length =40;
+for (let i =0; i<=length; i++){
+    console.log(fib(i));
+    // console.log(i+"番目は："+ fib(i));
+}
